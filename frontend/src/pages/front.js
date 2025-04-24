652f99006d23680d760bc67660e1e57765e74977
manimozhiyan23 <manimozhiyan88@gmail.com>
2025-04-24 07:15:00 +0530

    starting front page

---
+++ frontend/src/pages/front.js
@@ -0,0 +1,23 @@
+import "../styles/front.css";
+
+function Front(){
+    return(
+        <>
+        <div className="whole-background">
+
+            <div className="">
+
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+        </>
+    );
+}
+export default Front;
