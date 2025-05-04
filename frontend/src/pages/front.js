02c53c1d0a9f3dc879f1487e67b77c5bde897d3a
manimozhiyan23 <manimozhiyan88@gmail.com>
2025-05-04 12:05:12 +0530

    home page

--- frontend/src/pages/front.js
+++ frontend/src/pages/front.js
@@ -1,22 +1,38 @@
 import "../styles/front.css";
-
+import profil from "../images/profil.png"
 function Front(){
     return(
         <>
-        <div className="whole-background">
+        <div>
+        <div className="whole">
+          <div className="nav">
+            <p>Home</p>
+            <p>About</p>
+            <p>Edu & Exp</p>
+            <p>Contact</p>
+          </div>
 
-            <div className="">
-                
+          <div className="home-profil">
+            <div className="profil-image">
+                <img src={profil}/>
             </div>
-
-
-
-
-
-
-
-
+            <div className="hello">
+            <p>Hello, I'm</p>
+            <p className="hello-name">Manimozhiyan</p>
+            <p className="hello-resume">And this is my resume</p>
+            </div>
+        </div>    
         </div>
+
+       
+      
+      
+      
+      
+      
+      
+      
+      </div>
         </>
     );
 }
