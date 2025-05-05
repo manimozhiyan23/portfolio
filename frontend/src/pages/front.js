32a47a59ef5e673c13f08fd96514df1f19bb9209
manimozhiyan23 <manimozhiyan88@gmail.com>
2025-05-05 21:42:02 +0530

    about-page

--- frontend/src/pages/front.js
+++ frontend/src/pages/front.js
@@ -17,11 +17,42 @@ function Front(){
                 <img src={profil}/>
             </div>
             <div className="hello">
-            <p>Hello, I'm</p>
+            <p className="home-hello">Hello, I'm</p>
             <p className="hello-name">Manimozhiyan</p>
             <p className="hello-resume">And this is my resume</p>
             </div>
-        </div>    
+        </div>
+        <div className="about">
+        <div className="about-left">
+          <p className="about-hello">Hello!</p>
+          <p className="about-name">I'M Manimozhiyan</p>
+          <p className="about-me">
+          I am a passionate web developer specializing
+           in building modern, responsive, and user-friendly
+            web applications using React. With a strong
+             foundation in front-end development, I enjoy
+              turning ideas into functional and visually 
+              appealing digital experiences. I am skilled 
+              in designing and developing portfolio websites,
+               ensuring seamless performance, clean code, and
+                interactive UI components. I am always eager
+                 to learn new technologies and continuously
+                  improve my skills to deliver high-quality
+                  web solutions.
+          </p>
+          <div>
+          <button className="about-cv-download">cv download</button>
+        </div>
+        </div>
+        
+        <div className="about-image">
+                <img src={profil}/>
+            </div>
+        </div>
+          
+
+        
+         
         </div>
 
        
